ca7f89e722e96f5943d5d392cd22e4e42191b3e1
ponktacology <ponktacology@mailfence.com>
2025-02-27 16:28:32 +0100

    enforce proper media type for POST mappings

--- src/main/kotlin/me/wiktorlacki/ekomersz/user/auth/AuthenticationController.kt
+++ src/main/kotlin/me/wiktorlacki/ekomersz/user/auth/AuthenticationController.kt
@@ -1,11 +1,13 @@
 package me.wiktorlacki.ekomersz.user.auth
 
 import jakarta.validation.Valid
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.function.ServerResponse
 
 @RestController
 @RequestMapping("/api/auth")
@@ -13,13 +15,13 @@ class AuthenticationController(
     private val authenticationService: AuthenticationService
 ) {
 
-    @PostMapping("/login")
+    @PostMapping("/login", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun login(@RequestBody @Valid request: LoginRequest): ResponseEntity<LoginResponse> {
         val response = authenticationService.authenticate(request)
         return ResponseEntity.ok(response)
     }
 
-    @PostMapping("/register")
+    @PostMapping("/register", consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun register(@RequestBody @Valid request: RegistrationRequest): ResponseEntity<RegistrationResponse> {
         val response = authenticationService.register(request)
         return ResponseEntity.ok(response)
