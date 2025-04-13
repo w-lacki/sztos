8c83d55f7eb822b5707f63d7bdd46e151508b840
ponktacology <ponktacology@mailfence.com>
2025-04-13 02:28:55 +0200

    remove unused imports

--- backend/src/main/kotlin/me/wiktorlacki/stos/configuration/SecurityConfiguration.kt
+++ backend/src/main/kotlin/me/wiktorlacki/stos/configuration/SecurityConfiguration.kt
@@ -1,18 +1,16 @@
 package me.wiktorlacki.stos.configuration
 
-import org.springframework.boot.webservices.client.WebServiceMessageSenderFactory.http
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.config.annotation.web.invoke
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
